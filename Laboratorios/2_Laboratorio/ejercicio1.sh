f2353da77f1e27c038b3d25fa9b9cc272c044cc4
DG28828 <dannygarro28801@gmail.com>
2025-09-08 16:13:40 -0600

    Avance en ejercicio 1 de Lab 2

--- Laboratorios/2_Laboratorio/ejercicio1.sh
+++ Laboratorios/2_Laboratorio/ejercicio1.sh
@@ -5,7 +5,29 @@ usuario=$1
 grupo=$2
 ruta=$3
 
+#Crear archivo .log (eliminando el existente)
+if [[ -e registro_ejecucion.log ]]; then
+    sudo rm registro_ejecucion.log #Elimina el archivo si existe
+fi
+touch registro_ejecucion.log #Crea el archivo log para la ejecucion actual
+
+#Verificar usuario ROOT
 if [[ $EUID -ne 0 ]]; then #Verifica si el usuario actual no es root (si es root $EUID=0, si no es 1000)
     echo "ERROR: El usuario actual no es el usuario root. Ejecute el script desde el usuario root"
+    echo "ERROR: El usuario actual no es el usuario root. Finalizando ejecución" >> registro_ejecucion.log
     exit 2
 fi
+
+#Verificar si la ruta al archivo existe
+if ![[ -e $ruta ]]; then
+    echo "ERROR: La ruta no existe"
+    echo "ERROR: La ruta no existe. Finalizando ejecución" >> registro_ejecucion.log
+fi
+
+
+#Verificar si el grupo existe
+
+
+#Verificar si el usuario existe
+
+
