d471e2ec8fd3dddd475ff307d89b978205e858c9
DG28828 <dannygarro28801@gmail.com>
2025-09-26 17:57:50 -0600

    Creacion de rama y agregado el script inicial de ejercicio 2

---
+++ Laboratorios/3_Laboratorio/Ejercicio2/ejercicio2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int findLargestLine(int matrix[][SIZE]) {
+  // Su implementacion
+
+}
+
+int main() {
+  int matrix[SIZE][SIZE] = {
+    {2, 7, 6},
+    {9, 5, 1},
+    {4, 3, 8}
+  };
+
+  int largestLine = findLargestLine(matrix);
+  printf("El tamaño de la secuencia de ls mas grande es: %d\n", largestLine)
+    ;
+  return 0
+}
+
+  
