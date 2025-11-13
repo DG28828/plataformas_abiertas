3d9c6e12e707e64a92908a5809917018d86f7692
DG28828 <dannygarro28801@gmail.com>
2025-11-13 09:55:08 -0600

    Se ordenó el script y se modificó la distribucion del grid

--- proyecto/src/main.c
+++ proyecto/src/main.c
@@ -1,65 +1,84 @@
 #include <gtk/gtk.h>
 
 static void activate (GtkApplication* app, gpointer user_data) {
-  GtkWidget *window;
-  GtkWidget *grid;
-  GtkWidget *plot_area;
-  GtkWidget *plot_frame;
-  
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Visualizador de datos");
-  //gtk_window_set_default_size (GTK_WINDOW (window), 1200, 800);
-  //gtk_window_maximize(GTK_WINDOW (window));
 
-  grid = gtk_grid_new ();
+    // ***************  DECLARAR WIDGETS ***************
+    GtkWidget *window;       // Ventana principal
+    GtkWidget *grid;         // Grid (cuadricula base para contener otros widgets)
+    GtkWidget *plot_area;    // Area para colocar la grafica
+    GtkWidget *plot_frame;   // Marco para el area de la grafica
 
-  // Configurar cantidad de filas y columnas del grid
-  for (int i = 0; i<8; i++) {
-      gtk_grid_insert_row (GTK_GRID(grid), i);
-  }
-  for (int i = 0; i<3; i++) {
-      gtk_grid_insert_column (GTK_GRID(grid), i);
-  }
+    
+    // ***************  VENTANA PRINCIPAL ***************
+    // Crear ventana y asignar titulo
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Visualizador de datos");
 
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 3; j++) {
-        GtkWidget *label = gtk_label_new("Grid");
-        gtk_grid_attach(GTK_GRID(grid), label, j, i, 1, 1);
+    // Tamaño de ventana
+    //gtk_window_set_default_size (GTK_WINDOW (window), 1200, 800);
+    //gtk_window_maximize(GTK_WINDOW (window));
+
+    // Borde de ventana
+    gtk_container_set_border_width (GTK_CONTAINER (window), 25);
+
+    
+    // ***************        GRID        ***************
+    // Crear grid y definir cantidad de filas y columnas
+    grid = gtk_grid_new ();
+    for (int i = 0; i<3; i++) {                         // Filas del grid
+	gtk_grid_insert_row (GTK_GRID(grid), i);
     }
-  }
+    for (int i = 0; i<4; i++) {                         // Columnas del grid
+	gtk_grid_insert_column (GTK_GRID(grid), i);
+    }
+    
+    // (AUXILIAR, etiqueta al grid para visualizarlo)
+    for (int i = 0; i < 3; i++) {
+	for (int j = 0; j < 4; j++) {
+	    GtkWidget *label = gtk_label_new("Espacio");
+	    gtk_grid_attach(GTK_GRID(grid), label, j, i, 1, 1);
+	}
+    }
 
-  //Permitir tamaños distintos de filas y columnas del grid
-  gtk_grid_set_row_homogeneous(GTK_GRID(grid), FALSE);
-  gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
+    //Permitir tamaños distintos de filas y columnas del grid
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), FALSE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
 
-  gtk_container_add (GTK_CONTAINER (window), grid);
+    // Contener el grid en window
+    gtk_container_add (GTK_CONTAINER (window), grid);
 
-  plot_area = gtk_drawing_area_new ();
-  //gtk_widget_set_size_request(plot_area, 600, 400);
-  //gtk_widget_set_name(plot_area, "plot_area");
-  plot_frame = gtk_frame_new("Gráfica");
+    // ***************     PLOT AREA      ***************
+    //Crear plot area
+    plot_area = gtk_drawing_area_new ();
+    
+    // Tamaño del plot area
+    gtk_widget_set_size_request(plot_area, 1500, 600);
 
-  gtk_container_add(GTK_CONTAINER(plot_frame), plot_area);
+    // Nombre del plot area
+    //gtk_widget_set_name(plot_area, "plot_area");
 
-  //gtk_grid_attach(GTK_GRID(grid), plot_frame, 0, 4, 3, 4);
+    // Frame para el plot area
+    plot_frame = gtk_frame_new("Gráfica");
+    //gtk_widget_set_size_request(plot_frame, 1500, 600);
+    
+    // Contener plot area en el frame
+    gtk_container_add(GTK_CONTAINER(plot_frame), plot_area);
 
-
-  gtk_widget_set_size_request(plot_frame, 1000, 500);
-  gtk_grid_attach(GTK_GRID(grid), plot_frame, 0, 4, 3, 4);
+    // Contener el frame en el grid
+    gtk_grid_attach(GTK_GRID(grid), plot_frame, 0, 2, 4, 1);
   
-  gtk_container_set_border_width (GTK_CONTAINER (window), 25);
-  
-  gtk_widget_show_all (window);
+    // ***************  MOSTRAR RESULTADO  ***************
+    gtk_widget_show_all (window);
 }
 
 int main (int argc, char **argv) {
-  GtkApplication *app;
-  int status;
+    GtkApplication *app;
+    int status;
 
-  app = gtk_application_new ("org.gtk.DataVisualization", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+    app = gtk_application_new ("org.gtk.DataVisualization", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
 
-  return status;
+    return status;
 }
